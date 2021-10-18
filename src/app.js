8ae4306150aa0bc7165c542c27c648694cdb60c9
seymadonmez <77541232+seymadonmez@users.noreply.github.com>
2021-10-19 00:05:39 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,45 @@
+console.log("Merhaba kodlama.io") 
+
+//var dolarDun = 9.20
+//dolarDun = "Ankara"
+//var dolarBugun = 9.30
+//var dolarDun 
+//console.log(dolarDun)
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+//euroDun = 11 hata verir, const değişkenş atanınca başka atayamayız hata verir
+
+console.log(euroDun)
+
+//camelCasing
+
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"] //array
+
+console.log("<ul>")
+for (let i = 0; i < 3; i++) {
+    console.log("<li>"+ konutKredileri[i] + "</li>")
+}
+console.log("<ul>")
+
+{
+    // <ul>
+    //     <li>Konut kredisi</li>
+    //     <li>Emlak Konut Kredisi</li>
+    //     <li>Kamu Konut Kredisi</li>
+    // </ul>
+}
+
+console.log(konutKredileri)
+
+ 
